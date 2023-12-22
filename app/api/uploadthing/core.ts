e5f7a8ec21f0f58c9623ee963a55d41fdfb9a5de
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-22 11:32:00 +0300

    The file routers that link uploadthing to next app router.

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,21 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs";
+ 
+const uploadFunc = createUploadthing();
+ 
+// Middleware to handle auth, throws error if not authenticated.
+const handleAuth = () => {
+    const { userId } = auth()
+    // If you throw, the user will not be able to upload
+    if (!userId) throw new Error("Unauthorized");
+    return { userId };
+}
+ 
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  serverImage: uploadFunc({ image: { maxFileSize: "4MB", maxFileCount: 1 } }).middleware(() => handleAuth()).onUploadComplete(() => {}),
+  messageFile: uploadFunc(['image', 'pdf']).middleware(() => handleAuth()).onUploadComplete(() => {})
+} satisfies FileRouter;
+ 
+export type OurFileRouter = typeof ourFileRouter;
