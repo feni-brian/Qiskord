2cbeb06d4d32878dd94b9af445745b8276924168
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-23 13:30:52 +0300

    Get the current user's profile.

---
+++ lib/current-profile.ts
@@ -0,0 +1,8 @@
+import { auth } from "@clerk/nextjs";
+import { db } from "./db";
+
+export const currentProfile = async () => {
+	const { userId } = auth();
+	if (!userId) return null;
+	return await db.profile.findUnique({ where: { userId } });
+};
