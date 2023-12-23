87b9d07cbd26b88469bba299bb1b6865b20d63ea
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-23 13:31:33 +0300

    Get the user's profile page(s) from the current server.

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,9 @@
+import { getAuth } from "@clerk/nextjs/server";
+import { NextApiRequest } from "next";
+import { db } from "./db";
+
+export const currentProfilePages = async (request: NextApiRequest) => {
+	const { userId } = getAuth(request);
+	if (!userId) return null;
+	return await db.profile.findFirst({ where: { userId } });
+};
