feb6473c5f5d6f30d8088e415d79755bd512fb68
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-25 12:13:55 +0300

    Find, get, create or delete conversations in a server.

---
+++ lib/conversation.ts
@@ -0,0 +1,29 @@
+import { db } from "./db";
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+	try {
+		return await db.conversation.findFirst({
+			where: { AND: [{ memberOneId }, { memberTwoId }] },
+			include: { memberOne: { include: { profile: true } }, memberTwo: { include: { profile: true } } },
+		});
+	} catch {
+		return null;
+	}
+};
+
+const createConversation = async (memberOneId: string, memberTwoId: string) => {
+	try {
+		return await db.conversation.create({
+            data: { memberOneId, memberTwoId }, 
+            include: { memberOne: { include: { profile: true } }, memberTwo: { include: { profile: true } } }, 
+        });
+	} catch {
+		return null;
+	}
+};
+
+export const getOrCreateConversation = async (memberOneId: string, memberTwoId: string) => {
+	let conversation = (await findConversation(memberOneId, memberTwoId)) || (await findConversation(memberTwoId, memberOneId));
+	if (!conversation) conversation = await createConversation(memberOneId, memberTwoId);
+	return conversation;
+};
