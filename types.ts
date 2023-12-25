bed8204cb20379488ca6f9679e66d5119475b14e
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-25 12:15:33 +0300

    Supplement the Server component from @prisma/client with extra types.

---
+++ types.ts
@@ -0,0 +1,7 @@
+import { Member, Profile, Server } from "@prisma/client";
+import { Server as NetServer, Socket } from "net";
+import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
+
+export type NextApiResponseServerIO = NextApiResponse & { socket: Socket & { server: NetServer & { io: SocketIOServer } } };
+export type ServerWithMembersAndProfiles = Server & { members: (Member & { profile: Profile })[] };
