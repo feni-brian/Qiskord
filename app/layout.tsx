f9ec8bd86c5f903500526f91164949a05ff1f6d9
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-12-26 11:55:31 +0300

    Impemented the root layout structure of the app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -17,8 +20,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<ClerkProvider>
 			<html lang="en" suppressHydrationWarning>
 				<body className={cn(inter.className, "bg-white dark:bg-[#313338]")}>
-					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
-						{children}
+					<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="discord-theme">
+						<SocketProvider>
+							<ModalProvider />
+							<QueryProvider>{children}</QueryProvider>
+						</SocketProvider>
 					</ThemeProvider>
 				</body>
 			</html>
