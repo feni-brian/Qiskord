7df219126739311ce56f7f7f3ca072e121544009
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2024-01-01 15:34:56 +0300

    Show the audio or video chat room.

---
+++ components/media-room.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface MediaRoomProps {
+	chatId: string;
+	video: boolean;
+	audio: boolean;
+	params: {
+		serverId: string;
+		channelId: string;
+	};
+}
+
+export const MediaRoom = ({ chatId, video, audio, params }: MediaRoomProps) => {
+	const { user } = useUser();
+	const [token, setToken] = useState("");
+	const router = useRouter();
+	const redirectToGeneral = () => {
+		return router.push(`/servers/${params.serverId}/channels/${params.channelId}`);
+	};
+
+	useEffect(() => {
+		if (!user?.firstName || !user?.lastName) return;
+		const name = `${user.firstName} ${user.lastName}`;
+		(async () => {
+			try {
+				const response = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
+				const data = await response.json();
+				setToken(data.token);
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, [user?.firstName, user?.lastName, chatId]);
+
+	if (token === "")
+		return (
+			<div className="flex flex-col flex-1 justify-center items-center">
+				<Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
+				<p className="text-xs text-zinc-500 dark:text-zinc-400">Loading...</p>
+			</div>
+		);
+
+	return (
+		<LiveKitRoom data-lk-theme="default" token={token} connect={true} video={video} audio={audio} serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL} onDisconnected={redirectToGeneral}>
+			<VideoConference />
+		</LiveKitRoom>
+	);
+};
